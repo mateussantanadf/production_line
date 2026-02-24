d5fd3e755777d39beb74840a404e3ca700cf0f9e
santanamateus02 <santanamateus02@gmail.com>
2026-02-24 15:18:09 -0300

    create backend project

---
+++ production-backend/src/main/java/com/mateus/production_backend/ProductionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.production_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionBackendApplication.class, args);
+	}
+
+}
---
+++ production-backend/src/test/java/com/mateus/production_backend/ProductionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.production_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
